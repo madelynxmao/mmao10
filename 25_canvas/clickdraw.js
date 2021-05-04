46415d6bfbc954cfe20a73f7ed8af4159b5b0e2e
madelynxmao <mmao10@stuy.edu>
2021-05-04 19:46:08 -0400

    25

--- 25_canvas/clickdraw.js
+++ 25_canvas/clickdraw.js
@@ -1,52 +1,39 @@
+// Team m&a - Anya Zorin, Madelyn Mao
+// SoftDev
+// K25 --  I See a Red Door...
+// 2021-5-4
+
+let state = "dot";
+var c = document.getElementById("slate");
+var ctx = c.getContext("2d");
 
 
-canvas = document.getElementById('slate');
-ctx = canvas.getContext('2d');
-
-document.addEventListener("click", getClickPosition, false);
-
-clicked = true;
-
-function getClickPosition(e) {
-    var xPosition = e.clientX;
-    var yPosition = e.clientY;
+function changeShape(){
+    if (state=="dot"){state="rectangle"}
+    else{state="dot"}
+    return;
 }
 
-current = "draw";
+function empty(){
+    ctx.clearRect(0,0,600,600);
+}
 
-// every time button is clicked, mode changes from drawing-rectangle
-function draw_mode(){
-  if (current == "draw"){current = "rectangle";}
-  else{
-    if (current == "rectangle"){
-      current = "draw";
+c.addEventListener("click", (e)=>{
+    // e.offsetX gives the x coordinate of the mouse in the canvas
+    var x = e.offsetX;
+    // y.offsetX gives the y coordinate of the mouse in the canvas
+    var y = e.offsetY;
+    if (state=="dot"){
+        // beginPath is the equivilant to putting down a pen to start drawing
+        ctx.beginPath();
+        ctx.arc(x, y, 10, 0, 2 * Math.PI);
+        ctx.fill();
     }
-  }
-}
-
-// drawing and rectangle
-function main(){
-  if (current == "draw"){
-    console.log("drawing mode");
-
-    //make it so that the square is made where the user clicks
-
-    ctx.fillRect(xPosition,yPosition,1,1);
-
-  }
-  else{
-    console.log("rectangle mode");
-    ctx.fillStyle = 'green';
-
-
-    //make it so that the rectangle is made where the user clicked
-
-    ctx.fillRect(0, 0, xPosition, yPosition);
-  }
-}
-
-// clears board
-function clear(){
-    ctx.fillStyle = "#ffffff";
-    ctx.fillRect(0, 0, slate.width, slate.height);
-}
+    if (state=="rectangle"){
+        ctx.fillRect(x, y, 50, 30)
+    }
+    //preventDefault keeps the functions from doing what they normally do (ex; cant check a checkbox)
+    //in this case it does nothing
+    e.preventDefault();
+    return;
+});
